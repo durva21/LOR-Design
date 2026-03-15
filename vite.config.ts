7ee6b5902de5268925e564e80e6e08b84e0cffc0
durva21 <durvabadekar21@gmail.com>
2026-03-15 20:38:57 +0530

    fix base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-   base: '/LOR-Design/',
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
